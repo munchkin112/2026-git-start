5b8f6e7dc26609a821e38db4305270f77176e808
MUNCHKIN <dlrgusdl12345@gmail.com>
2026-08-11 20:34:14 +0900

    feat: center greeting text

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 
 function App() {
-  return null
+  return <main>안녕하세요</main>
 }
 
 createRoot(document.getElementById('root')).render(
